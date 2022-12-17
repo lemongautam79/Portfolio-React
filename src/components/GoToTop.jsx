494659652f6607ae6a8b51cc06da52a00aa1ebe5
lemongautam79 <87838630+lemongautam79@users.noreply.github.com>
2022-12-17 23:59:05 +0545

    Didn't work

--- src/components/GoToTop.jsx
+++ src/components/GoToTop.jsx
@@ -1,76 +1,60 @@
 import React, { useState, useEffect } from 'react'
 
 const GoToTop = () => {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const goToBtn = () => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    };
+  const goToBtn = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
-    const listenToScroll = () => {
-        let heightToHidden = 20;
-        const winScroll =
-            document.body.scrollTop || document.documentElement.scrollTop;
+  const listenToScroll = () => {
+    let heightToHidden = 20;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
 
-        if (winScroll > heightToHidden) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-    };
+    if (winScroll > heightToHidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll", listenToScroll);
-        return () => window.removeEventListener("scroll", listenToScroll);
-    }, []);
-    return (
-        <div className="wrapper">
-            {isVisible && (
-                <div class="top-btn" onClick={goToBtn}>
-                    <h6>Top</h6>
-                </div>
-            )}
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "relative",
+    }}>
+      {isVisible && (
+        <div style={{
+          fontSize: "2.4rem",
+          width: "6rem",
+          height: "6rem",
+          color: "#fff",
+          backgroundColor: "#ff5003",
+          // boxShadow: "${({ theme }) => theme.colors.shadow}",
+          borderRadius: "50%",
+          position: "fixed",
+          bottom: "5rem",
+          right: "5rem",
+          zIndex: "999",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          cursor: "pointer"
+        }}
+          onClick={goToBtn}>
+          <i class="bi bi-arrow-up" style={{}}></i>
         </div>
-    )
-    const Wrapper = styled.section`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    .top-btn {
-    font-size: 2.4rem;
-    width: 6rem;
-    height: 6rem;
-    color: #fff;
-    background-color: ${({ theme }) => theme.colors.btn};
-    box-shadow: ${({ theme }) => theme.colors.shadow};
-    border-radius: 50%;
-    position: fixed;
-    bottom: 5rem;
-    right: 5rem;
-    z-index: 999;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    &--icon {
-      animation: gototop 1.2s linear infinite alternate-reverse;
-    }
-    @keyframes gototop {
-      0% {
-        transform: translateY(-0.5rem);
+      )
       }
-      100% {
-        transform: translateY(1rem);
-      }
-    }
-  }
-  @media (max-width: ${({ theme }) => theme.media.mobile}) {
-    .top-btn {
-      right: 0;
-      left: 40%;
-    }
-  }
+    </div >
+  )
 }
 
 export default GoToTop
