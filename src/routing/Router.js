fd3d030286e980f0c84636fa5be34952672cb768
lemongautam79 <87838630+lemongautam79@users.noreply.github.com>
2022-12-17 20:38:38 +0545

    Scroll to top

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GoToTop from '../components/GoToTop'
 import ErrorPage from '../Pages/ErrorPage'
 import HomePage from '../Pages/HomePage'
 
 const Router = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="*" element={<ErrorPage />} />
+                </Routes>
+            </BrowserRouter>
+            <GoToTop />
+        </>
     )
 }
 
