7585041bf251a9afe62aea96b4380e21a6667407
lemongautam79 <87838630+lemongautam79@users.noreply.github.com>
2023-02-27 18:43:52 +0545

    GD logo

--- src/components/CompaniesLogo.jsx
+++ src/components/CompaniesLogo.jsx
@@ -4,41 +4,45 @@ import Unarlogo from '../images/projects/Other files/Unarlogo.jpg'
 import { useTranslation } from 'react-i18next'
 
 const CompaniesLogo = () => {
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     return (
         <>
             {/* <!-- //! Companies logo --> */}
             <section class="clients section-padding">
-                <div  class="container">
-                    <div  data-aos="fade-up" data-aos-offset="200" data-aos-duration = "600" class="row align-items-center">
+                <div class="container">
+                    <div data-aos="fade-up" data-aos-offset="200" data-aos-duration="600" class="row align-items-center">
 
                         <div class="col-lg-12 col-12">
                             <h3 class="text-center mb-5">{t('companies_work')}</h3>
                         </div>
 
-                        <div class="col-lg-2 col-4 ms-auto clients-item-height">
+                        {/* <div class="col-lg-2 col-4 ms-auto clients-item-height">
                             <a href="https://developergd.com/" target="_blank">
                                 <img src={GDIcon} class="clients-image img-fluid" alt="GDIcon" loading="lazy" />
                             </a>
+                        </div> */}
+
+                        {/* //! Horizontally centered logo */}
+                        <div class="col-lg-2 col-4 mx-auto clients-item-height ">
+                            <a href="https://developergd.com/" target="_blank">
+                                <img src={GDIcon} class="clients-image img-fluid " alt="GDIcon" loading="lazy" />
+                            </a>
                         </div>
 
-                        {/* <!-- <div class="col-lg-2 col-4 clients-item-height">
-                            <img src="images/clients/guitar-center.svg" class="clients-image img-fluid" alt="">
+                        {/* <div class="col-lg-2 col-4 clients-item-height">
+                            <img src="images/clients/tokico.svg" class="clients-image img-fluid" alt="" />
                         </div>
 
                         <div class="col-lg-2 col-4 clients-item-height">
-                            <img src="images/clients/tokico.svg" class="clients-image img-fluid" alt="">
-                        </div>
+                            <img src="images/clients/shopify.svg" class="clients-image img-fluid" alt="" />
+                        </div> */}
 
-                        <div class="col-lg-2 col-4 clients-item-height">
-                            <img src="images/clients/shopify.svg" class="clients-image img-fluid" alt="">
-                        </div> --> */}
-
-                        <div  class="col-lg-2 col-4 me-auto clients-item-height">
+                        {/* <div  class="col-lg-2 col-4 me-auto clients-item-height">
                             <a href="https://unarstore.com/#/" target="_blank">
                                 <img src={Unarlogo} class="clients-image img-fluid" alt="Unarlogo" loading="lazy" />
                             </a>
-                        </div>
+                        </div> */}
+
                     </div>
                 </div>
             </section>
