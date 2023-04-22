551f04e2c5b5a7302f67e4282ba3e709c44da743
lemongautam79 <87838630+lemongautam79@users.noreply.github.com>
2023-04-22 08:37:18 +0545

    Auto date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next'
 
 const Footer = () => {
     const { t } = useTranslation()
+    const date = new Date();
+    const year = date.getFullYear();
     return (
         <>
             <footer class="site-footer">
@@ -12,7 +14,7 @@ const Footer = () => {
                         <div class="col-lg-12 col-12">
                             <div class="copyright-text-wrap">
                                 <p class="mb-0">
-                                    <span class="copyright-text">{t('copyright')}</span>
+                                    <span class="copyright-text">{t('copyright')+year+t('remaining_text')}</span>
                                     <a rel="sponsored" href="/" target="_blank">{t('full_name')}</a>
                                 </p>
                             </div>
